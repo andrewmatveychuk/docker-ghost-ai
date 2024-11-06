b528543599ee701c48917b641f0768a75885929a
Andrew Matveychuk <andrew@andrewmatveychuk.com>
2024-11-06 14:16:09 +0100

    Switched to using separate parameters for host and port chack by wait-for-it

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,15 +3,15 @@
 # Abort on any error (including if wait-for-it fails).
 set -e
 
-# Wait for the MySQL backend to be up
+# Wait for the batabase backend to be up
 
 # If both database connection host and port are set
 if [ -n "$database__connection__host" ] && [ -n "$database__connection__port" ]; then
-    ./wait-for-it.sh "$database__connection__host:$database__connection__port" --strict --timeout=300
+    ./wait-for-it.sh --host="$database__connection__host" --port="$database__connection__port" --strict --timeout=300
 fi
 # If only database connection host is set
 if [ -n "$database__connection__host" ] && [ -z "$database__connection__port" ]; then
-    ./wait-for-it.sh "$database__connection__host" --strict --timeout=300
+    ./wait-for-it.sh --host="$database__connection__host" --strict --timeout=300
 fi
 
 # Run the main container command
