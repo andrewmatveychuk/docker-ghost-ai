d2078540816da8f5c87d03013f62b906e7715a96
Andrew Matveychuk <andrew@andrewmatveychuk.com>
2024-11-11 13:21:39 +0100

    Removed unsupported configuration methods in Application Insights

--- ai-bootstrap.js
+++ ai-bootstrap.js
@@ -4,15 +4,17 @@ const connectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING;
 if (connectionString) {
     console.log('Using Application Insights connection string.');
     appInsights.setup(connectionString)
-        .setAutoCollectRequests(true)
-        .setAutoCollectPerformance(true, true)
-        .setAutoCollectExceptions(true)
-        .setAutoCollectDependencies(true)
-        .setAutoCollectConsole(true, false)
-        .setAutoCollectPreAggregatedMetrics(true)
-        .setSendLiveMetrics(false)
-        .setInternalLogging(false, true)
+        .enableLiveMetrics(true)
         .enableWebInstrumentation(true)
+        // .setAutoCollectRequests(true)
+        // .setAutoCollectPerformance(true, true)
+        // .setAutoCollectExceptions(true)
+        // .setAutoCollectDependencies(true)
+        // .setAutoCollectConsole(true, false)
+        // .setAutoCollectPreAggregatedMetrics(true)
+        // .setSendLiveMetrics(false)
+        // .setInternalLogging(false, true)
+        // .enableWebInstrumentation(true)
 
     appInsights.start();
 } else {
