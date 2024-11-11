8783635a2352a009756f53c05c27bb1bd1f64768
Andrew Matveychuk <andrew@andrewmatveychuk.com>
2024-11-11 15:00:29 +0100

    Updated configuration settings for Application Insights

--- ai-bootstrap.js
+++ ai-bootstrap.js
@@ -4,10 +4,14 @@ const connectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING;
 if (connectionString) {
     console.log('Using Application Insights connection string.');
     appInsights.setup(connectionString)
-        .enableLiveMetrics(true)
-        .enableWebInstrumentation(true);
-
-    appInsights.start();
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true, true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true, true)
+        .enableWebInstrumentation(true)
+        .start();
 } else {
     console.log('WARNING: Application Insights connection string is not set.');
 }
