10126d8e5d839408f44457b9d346d2dd3e41d93e
Andrew Matveychuk <andrew@andrewmatveychuk.com>
2024-11-12 11:19:13 +0100

    Removed optional configuration settings

--- ai-bootstrap.js
+++ ai-bootstrap.js
@@ -3,15 +3,7 @@ const connectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING;
 
 if (connectionString) {
     console.log('Using Application Insights connection string.');
-    appInsights.setup(connectionString)
-        .setAutoDependencyCorrelation(true)
-        .setAutoCollectRequests(true)
-        .setAutoCollectPerformance(true, true)
-        .setAutoCollectExceptions(true)
-        .setAutoCollectDependencies(true)
-        .setAutoCollectConsole(true, true)
-        .enableWebInstrumentation(true)
-        .start();
+    appInsights.setup(connectionString).start();
 } else {
     console.log('WARNING: Application Insights connection string is not set.');
 }
